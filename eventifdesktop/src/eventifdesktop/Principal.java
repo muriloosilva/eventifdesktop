445e1a84d6c3bf9a2268084d2b7a52d3c020139d
murilo <muriloosilva@gmail.com>
2015-05-21 09:58:34 -0300

    Merge com o modulo02

--- eventifdesktop/src/eventifdesktop/Principal.java
+++ eventifdesktop/src/eventifdesktop/Principal.java
@@ -3,8 +3,11 @@ package eventifdesktop;
 public class Principal {
 
 	public static void main(String[] args) {
+		message();
+	}
+	
+	public static void message(){
 		System.out.println("Ola Mundo!");
-
 	}
 
 }
